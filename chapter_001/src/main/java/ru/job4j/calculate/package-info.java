4940ff9c8db9438fc7056743363adb476896f1b7
aaawwweee <kashkinmsk@gmail.com>
2018-12-21 00:17:14 +0300

    1.3. JavaDoc. Code convention[#98110]

--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -1,15 +1,14 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Alexander Kashkin (kashkinmsk@gmail.com)
+ * @version 1
+ * @since 21.12.2018
+ */
 package ru.job4j.calculate;
 
-/**
- * Calculate.
- * 
- * @author Alexander Kashkin (kashkinmsk@gmail.com)
- */
 public class Calculate {
-    /**
-     * Main.
-	 * @param args - args.
-	 */
+
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }

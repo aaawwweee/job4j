fe87c90e65d231fc11496e760ec7aa573e020e36
aaawwweee <kashkinmsk@gmail.com>
2018-12-23 18:56:27 +0300

    1.4. JUnit[#98111]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -14,4 +14,12 @@ public class Calculate {
         System.out.println("Hello, World");
     }
 
+/**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+    }
 }

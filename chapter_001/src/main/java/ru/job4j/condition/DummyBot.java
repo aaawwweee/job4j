b31fd5cf27b2a9f4a96dd52ea9773e59f1923b90
aaawwweee <kashkinmsk@gmail.com>
2019-01-01 21:01:17 +0300

    4.1. Глупый бот.[#98073]

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -7,6 +7,11 @@ package ru.job4j.condition;
  * @version 1
  */
 public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
     public String answer (String question){
         String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
         if ("Привет, Бот.".equals(question)) {

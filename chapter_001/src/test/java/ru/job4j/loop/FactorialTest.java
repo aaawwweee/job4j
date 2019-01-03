4e66e8a1721c7d6b66acf2d5cc330ccc1622df47
aaawwweee <kashkinmsk@gmail.com>
2019-01-03 12:33:58 +0300

    5.2. Создать программу вычисляющую факториал.[#98082](add Factorialfrom0then1)

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -6,11 +6,17 @@ import static org.junit.Assert.assertThat;
 
 public class FactorialTest {
     @Test
-    public void Factorialfrom5Then120(){
-        Factorial fact = new Factorial ();
+    public void Factorialfrom5Then120() {
+        Factorial fact = new Factorial();
         int result = fact.calculateFactorial(5);
         int expect = 120;
-        assertThat(result,is (expect));
-        }
-
+        assertThat(result, is(expect));
     }
+    @Test
+    public void Factorialfrom0then1() {
+        Factorial fact = new Factorial();
+        int result = fact.calculateFactorial(0);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+}

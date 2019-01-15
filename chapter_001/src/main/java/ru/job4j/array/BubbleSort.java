0735adfb4fa76faa6ab5654ffed299bcd952f97f
aaawwweee <kashkinmsk@gmail.com>
2019-01-15 08:27:19 +0300

    BubbleSort Addition 2

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
     public int[] sort(int[] array) {
         int temp;
         for (int i = array.length - 1; i > 0; i--) {
-            for (int j = 0; j < array.length - 1; j++) {
+            for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
                     temp = array[j];
                     array[j] = array[j + 1];

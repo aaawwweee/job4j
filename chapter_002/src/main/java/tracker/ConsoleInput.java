e7f8034ecef8102a272794e4db84ecb1f82f50fc
aaawwweee <kashkinmsk@gmail.com>
2019-02-05 07:22:33 +0300

    Создания Manifest для проекта Tracker[#98147]

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
-    public String ask(String question){
+    public String ask(String question) {
         System.out.println(question);
         return scanner.nextLine();
     }
--- chapter_002/src/main/java/tracker/StartUI.java
+++ chapter_002/src/main/java/tracker/StartUI.java
@@ -6,11 +6,11 @@ public class StartUI {
     }
 
     private static final String ADD = "0";
-    private static final String ShowAll = "1";
+    private static final String SHOWALL = "1";
     private static final String EDIT = "2";
     private static final String DELETE = "3";
-    private static final String FindById = "4";
-    private static final String FindByName = "5";
+    private static final String FINDBYID = "4";
+    private static final String FINDBYNAME = "5";
     private static final String EXIT = "6";
     private final Input input;
     private final Tracker tracker;
@@ -21,13 +21,13 @@ public class StartUI {
     }
 
     private void showMenu() {
-        System.out.println("0. Add new Item\n" +
-                "1.Show all items\n" +
-                "2. Edit Item\n" +
-                "3. Delete item\n" +
-                "4. Find items by Id\n" +
-                "5. Find item by names\n" +
-                "6. Exit Program\n");
+        System.out.println("0. Add new Item\n"
+                + "1.Show all items\n"
+                + "2. Edit Item\n"
+                + "3. Delete item\n"
+                + "4. Find items by Id\n"
+                + "5. Find item by names\n"
+                + "6. Exit Program\n");
     }
 
     private void createItem() {
@@ -60,14 +60,14 @@ public class StartUI {
         }
     }
 
-    private void FindItemById() {
+    private void findItemById() {
         System.out.println("---------- Поиск заявки по ID ----------");
         String id = this.input.ask("Введите ID заявки: ");
         Item item = this.tracker.findById(id);
         System.out.println("ID заявки: " + item.getId() + " Имя заявки: " + item.getName() + " Описание заявки: " + item.getDesc());
     }
 
-    private void FindItemsByName() {
+    private void findItemsByName() {
         System.out.println("---------- Поиск заявки по имени ----------");
         String name = this.input.ask("Введите имя заявки: ");
         Item[] items = this.tracker.findByName(name);
@@ -86,27 +86,26 @@ public class StartUI {
             System.out.println("---------- Заявка не удалена ----------");
         }
     }
-        public void init () {
+        public void init() {
             boolean exit = false;
             while (!exit) {
                 this.showMenu();
                 String answer = this.input.ask("Select:");
                 if (ADD.equals(answer)) {
                     this.createItem();
-                } else if (ShowAll.equals(answer)) {
+                } else if (SHOWALL.equals(answer)) {
                     this.showItems();
                 } else if (EDIT.equals(answer)) {
                     this.editItems();
-                } else if (FindById.equals(answer)) {
-                    this.FindItemById();
+                } else if (FINDBYID.equals(answer)) {
+                    this.findItemById();
                 } else if (DELETE.equals(answer)) {
                     this.deleteItems();
-                } else if (FindByName.equals(answer)) {
-                    this.FindItemsByName();
+                } else if (FINDBYNAME.equals(answer)) {
+                    this.findItemsByName();
                 } else if (EXIT.equals(answer)) {
                     exit = true;
                 }
             }
         }
     }
-}

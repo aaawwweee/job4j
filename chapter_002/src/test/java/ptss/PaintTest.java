ef874b4056f211a735a0120c5e28a12ef7685c0a
aaawwweee <kashkinmsk@gmail.com>
2019-02-10 10:58:03 +0300

    Используя шаблон проектирования - стратегию v2[#98150]

--- chapter_002/src/test/java/ptss/PaintTest.java
+++ chapter_002/src/test/java/ptss/PaintTest.java
@@ -27,4 +27,25 @@ public class PaintTest {
         );
         System.setOut(stdout);
     }
+
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("   +   ")
+                                .append("  +++  ")
+                                .append(" +++++ ")
+                                .append("+++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        System.setOut(stdout);
+    }
 }

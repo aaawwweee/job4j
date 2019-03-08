f5dd39923ea6d99c6f960de886fea6559a4e2be7
aaawwweee <kashkinmsk@gmail.com>
2019-03-08 21:20:36 +0300

    Вычисление площади треугольника v2[#119254]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -24,10 +24,11 @@ public class Triangle {
      * @return area
      */
     public double area(int x1, int x2, int y1, int y2, int x3, int y3) {
+        Point point = new Point();
         double rst = -1;
-        double a = new Point().distance(x1, y1, x2, y2);
-        double b = new Point().distance(x2, y2, x3, y3);
-        double c = new Point().distance(x1, y1, x3, y3);
+        double a = point.distance(x1, y1, x2, y2);
+        double b = point.distance(x2, y2, x3, y3);
+        double c = point.distance(x1, y1, x3, y3);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             rst = Math.sqrt(p * (p - a) * (p - b) * (p - c));

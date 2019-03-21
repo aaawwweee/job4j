7a98ca0dd3ceb09aa95be1fdcf15a538f5decf78
aaawwweee <kashkinmsk@gmail.com>
2019-03-21 21:52:03 +0300

    if-else-throw[#119337]

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -23,10 +23,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 }

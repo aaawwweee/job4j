958bc58da803eef98c0eb94d0083ec79fabf6fbc
aaawwweee <kashkinmsk@gmail.com>
2019-04-01 23:00:04 +0300

    Очередь с приоритетом на LinkedList[#119317]

---
+++ chapter_002/src/main/java/search/PriorityQueue.java
@@ -0,0 +1,27 @@
+package search;
+import java.util.LinkedList;
+
+/**
+ * @author Alexander Kashkin
+ * @since 01.04.2019
+ * @version 1
+ */
+public class PriorityQueue {
+
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int x = tasks.size();
+        for (int i = 0; i < x; i++) {
+            if (task.getPriority() < tasks.get(i).getPriority()) {
+                x = tasks.indexOf(tasks.get(i));
+                break;
+            }
+        }
+        this.tasks.add(x, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}

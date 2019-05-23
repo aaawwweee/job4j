def993da31e696d49ce67e3fd0fca34209c43f25
aaawwweee <kashkinmsk@gmail.com>
2019-05-23 15:35:11 +0300

    Создать метод delete для односвязного списка[#142249]

---
+++ chapter_002/src/main/java/list/SimpleArrayList.java
@@ -0,0 +1,43 @@
+package list;
+import java.util.NoSuchElementException;
+/**
+ * @author Alexander Kashkin (kashkinmsk@gmail.com)
+ * @since 23.05.2019
+ * @version 1
+ */
+public class SimpleArrayList<E> {
+    private int size;
+    private Node<E> first;
+
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+    public E delete(){
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        this.first = first.next;
+        this.size--;
+        return this.first.data;
+    }
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+    public int getSize() {
+        return this.size;
+    }
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}

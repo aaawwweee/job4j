2f587e455e6720d02759d383fdb5ef115f09e902
aaawwweee <kashkinmsk@gmail.com>
2019-06-03 12:30:53 +0300

    Создать модель User[#142258]

---
+++ chapter_002/src/main/java/map/User.java
@@ -0,0 +1,45 @@
+package map;
+import java.util.Calendar;
+
+/**
+ * Class User
+ * @author Alexander Kashkin (kashkinmsk@gmail.com)
+ * @since 03.06.2019
+ * @version 1
+ */
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}

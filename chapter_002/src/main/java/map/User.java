a9cced07509dd8b0e2b5f3c0c614684a24239084
aaawwweee <kashkinmsk@gmail.com>
2019-06-04 12:14:38 +0300

    Переопределить только equals[#142261]

--- chapter_002/src/main/java/map/User.java
+++ chapter_002/src/main/java/map/User.java
@@ -45,10 +45,14 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + children;
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
     }
 }

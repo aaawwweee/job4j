f03063688cd48c760703784d7649670a4cdec121
aaawwweee <kashkinmsk@gmail.com>
2019-07-10 12:04:12 +0300

    Анализ доступности сервера.[#142290]

--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -2,23 +2,26 @@ package io;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
-import org.junit.Before;
 import org.junit.Test;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.PrintWriter;
 
 public class AnalizyTest {
-    public Analizy analizy;
-
-    @Before
-    public void init() {
-        analizy = new Analizy();
-    }
-
     @Test
     public void whenItsWorking() throws Exception {
+        Analizy analizy = new Analizy();
         String source = System.getProperty("java.io.tmpdir") + "server.log";
         String target = System.getProperty("java.io.tmpdir")+ "unavailable.csv";
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(source))) {
+            out.println("200 10:56:01");
+            out.println("200 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
         analizy.unavailable(source, target);
         BufferedReader reader = new BufferedReader(new FileReader(target));
         assertThat(reader.readLine(), is("10:58:01;10:59:01"));
